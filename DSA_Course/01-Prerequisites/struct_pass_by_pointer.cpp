98d429209aa499cea054f6a374620440a14872c4
Bhavy Jain <bhavy.delhi@gmail.com>
2026-08-05 03:00:38 +0530

    Implement structure pass by pointer example

---
+++ DSA_Course/01-Prerequisites/struct_pass_by_pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct Rectangle {
+    int length;
+    int breadth;
+};
+
+void fun(Rectangle* r) {
+    r->length = 7;
+    r->breadth = 8;
+
+    cout << r->length << endl;
+    cout << r->breadth << endl;
+}
+
+int main() {
+    Rectangle r = {10, 5};
+
+    fun(&r);
+
+    cout << r.length << endl;
+    cout << r.breadth << endl;
+
+    return 0;
+}
