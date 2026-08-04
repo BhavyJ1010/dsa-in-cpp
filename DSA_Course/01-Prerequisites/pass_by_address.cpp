757888ad380c951c76b2005a60d56533d8b04278
Bhavy Jain <bhavy.delhi@gmail.com>
2026-08-04 14:50:28 +0530

    Implement pass by address example

---
+++ DSA_Course/01-Prerequisites/pass_by_address.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* x, int* y) {
+    // Original variables are modified
+
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+
+    cout << "x = " << *x << endl;
+    cout << "y = " << *y << endl;
+
+    cout << endl;
+}
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    swap(&a, &b);
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
