5e84d3973e172bfbd7e5a0f3211836023f8dd712
Bhavy Jain <bhavy.delhi@gmail.com>
2026-08-08 01:31:45 +0530

    Add iterative sum example

---
+++ DSA_Course/02-Recursion/07_sum_loop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n) {
+    int total = 0;
+
+    for (int i = 1; i <= n; i++) {
+        total += i;
+    }
+
+    return total;
+}
+
+int main() {
+    int n;
+    cout << "Enter a natural number: ";
+    cin >> n;
+
+    cout << "Sum = " << sum(n) << endl;
+
+    return 0;
+}
