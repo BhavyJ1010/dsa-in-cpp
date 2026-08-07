0958f6cfbf2df7687a63a6e07f31d89505cbefa3
Bhavy Jain <bhavy.delhi@gmail.com>
2026-08-08 01:35:44 +0530

    Implement recursive exponentiation example

---
+++ DSA_Course/02-Recursion/10_exponent_recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int power(int x, int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    if (n % 2 == 0) {
+        return power(x * x, n / 2);
+    }
+
+    return x * power(x * x, (n - 1) / 2);
+}
+
+int main() {
+    cout << power(5, 4);
+
+    return 0;
+}
