9f4c255bdbf42693cec3b6324faf271317d49069
Bhavy Jain <bhavy.delhi@gmail.com>
2026-08-08 01:30:48 +0530

    Implement recursive sum example

---
+++ DSA_Course/02-Recursion/06_sum_recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n) {
+    if (n == 0) {
+        return 0;
+    }
+
+    return n + sum(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter a natural number: ";
+    cin >> n;
+
+    cout << "Sum = " << sum(n) << endl;
+
+    return 0;
+}
